72667677a7a38efc8317c20cc6129b09941ecb5b
Egor Kolesnikov <freethinkel@gmail.com>
2023-07-14 10:52:19 +0600

    fix utils lowercase

--- lua/oshmes/utils.lua
+++ lua/oshmes/utils.lua
@@ -1,13 +1,13 @@
-local m = {}
+local M = {}
 
-m.hi = function(name, options)
+M.hi = function(name, options)
   if (type(options) == "string") then
     vim.cmd('hi! link ' .. name .. " " .. options)
     return
   end
   vim.api.nvim_set_hl(0, name, options)
 
-  -- vim.api.nvim_create_autocmd({ "colorscheme" }, {
+  -- vim.api.nvim_create_autocmd({ "ColorScheme" }, {
   --   pattern = { "*" },
   --   callback = function()
   --     vim.api.nvim_set_hl(0, name, options)
@@ -15,13 +15,13 @@ m.hi = function(name, options)
   -- })
 end
 
-m.load_highlights = function(highlights)
+M.load_highlights = function(highlights)
   for key, value in pairs(highlights) do
-    m.hi(key, value)
+    M.hi(key, value)
   end
 end
 
-local hextorgb = function(color)
+local hexToRgb = function(color)
   return {
     tonumber(string.sub(color, 2, 3), 16),
     tonumber(string.sub(color, 4, 5), 16),
@@ -29,8 +29,8 @@ local hextorgb = function(color)
   }
 end
 
-local _mixcolorchannel = function(channela, channelb, amount)
-  return (channela * amount) + (channelb * (1 - amount))
+local _mixColorChannel = function(channelA, channelB, amount)
+  return (channelA * amount) + (channelB * (1 - amount))
 end
 
 local tohex = function(num)
@@ -42,26 +42,26 @@ local tohex = function(num)
   return '0' .. val
 end
 
-m.darken = function(color, amount)
-  return m.mixcolors(color, "#000000", amount)
+M.darken = function(color, amount)
+  return M.mixColors(color, "#000000", amount)
 end
 
-m.lighten = function(color, amount)
-  return m.mixcolors(color, "#ffffff", amount)
+M.lighten = function(color, amount)
+  return M.mixColors(color, "#ffffff", amount)
 end
 
-m.mixcolors = function(colora, colorb, amount)
-  colora = hextorgb(colora)
-  colorb = hextorgb(colorb)
+M.mixColors = function(colorA, colorB, amount)
+  colorA = hexToRgb(colorA)
+  colorB = hexToRgb(colorB)
   amount = 1 - amount
   local mixed = {
-    _mixcolorchannel(colora[1], colorb[1], amount),
-    _mixcolorchannel(colora[2], colorb[2], amount),
-    _mixcolorchannel(colora[3], colorb[3], amount),
+    _mixColorChannel(colorA[1], colorB[1], amount),
+    _mixColorChannel(colorA[2], colorB[2], amount),
+    _mixColorChannel(colorA[3], colorB[3], amount),
   }
 
   return "#" .. tohex(mixed[1]) .. tohex(mixed[2]) .. tohex(mixed[3])
 end
 
 
-return m
+return M
